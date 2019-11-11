1e5343231f0ed5a5151d0946a230ba419a203006
suxiting <xitingsu@jieyuechina.com>
2019-11-11 18:09:21 +0800

    12

--- app/src/main/java/com/study/mylivedata/MainVM.java
+++ app/src/main/java/com/study/mylivedata/MainVM.java
@@ -21,43 +21,7 @@ public class MainVM extends BaseViewModel {
     private LiveData banners;
 
     public MainVM(){
-        this.bannerList = Transformations.switchMap(this.getPage(), new Function() {
-            @Override
-            public Object apply(Object input) {
-                return this.apply(input);
-            }
 
-            public final LiveData apply(Integer it) {
-                LiveData liveData;
-                if (it != null) {
-                    if (it == 0) {
-                        liveData = MainVM.this.getApi().bannerList();
-                        return liveData;
-                    }
-                }
-                liveData = AbsentLiveData.Companion.create();
-                return liveData;
-            }
-        }));
-        LiveData liveData = Transformations.map(this.bannerList, (Function)(new Function() {
-            // $FF: synthetic method
-            // $FF: bridge method
-            public Object apply(Object var1) {
-                return this.apply((BaseResponseBean)var1);
-            }
-
-            public final List apply(BaseResponseBean it) {
-                MainVM.this.getLoading().setValue(false);
-                List list = (List)it.getData();
-                if (list == null) {
-                    list = (List)(new ArrayList());
-                }
-
-                return list;
-            }
-        }));
-        Intrinsics.checkExpressionValueIsNotNull(liveData, "Transformations.map(bann…data ?: ArrayList()\n    }");
-        this.banners = liveData;
     }
 
 
--- app/src/main/java/com/study/mylivedata/base/BaseViewModel.java
+++ app/src/main/java/com/study/mylivedata/base/BaseViewModel.java
@@ -9,19 +9,12 @@ import org.jetbrains.annotations.NotNull;
 
 public class BaseViewModel extends ViewModel {
 
-    public MutableLiveData refreshTrigger = new MutableLiveData();
     public MutableLiveData loading = new MutableLiveData();
-    private MutableLiveData page;
+
     private  RetrofitAndLiveDataManager api;
 
     public BaseViewModel(){
         this.api = (RetrofitAndLiveDataManager) api.getApiService();
-        this.page = new MutableLiveData();
-    }
-
-
-    public  MutableLiveData getRefreshTrigger() {
-        return this.refreshTrigger;
     }
 
     public  MutableLiveData getLoading() {
@@ -32,12 +25,7 @@ public class BaseViewModel extends ViewModel {
         return this.api;
     }
 
-    protected final MutableLiveData getPage() {
-        return this.page;
-    }
-
     public void loadData() {
-        this.refreshTrigger.setValue(true);
         this.loading.setValue(true);
     }
 
